6238bb7bc10b12f1d9093225ea597d33183959ca
Kush Oza <kko15@cd.cs.rutgers.edu>
2016-03-27 19:12:38 -0400

    Added partner changes

--- stage3.c
+++ stage3.c
@@ -2,19 +2,47 @@
 #include "stat.h"
 #include "user.h"
 #include "signal.h"
+//#include "proc.h"
+
+static float trapCount = 0.0f;
+static float time = 0.0f;
 
 void handle_signal(siginfo_t info)
 {
+	printf(1, "Caught signal %d...\n", info.signum);
+	if (info.signum == SIGFPE)
+		printf(1, "TEST PASSED\n");
+	else
+		printf(1, "TEST FAILED: wrong signal sent.\n");
 
+	if(trapCount < 10.0f){
+		trapCount=trapCount+1.0;
+		printf(1,"%d",trapCount);
+	}
+	else{
+		__asm__(
+			"movl %eax, %esp\n\t"
+			"add 4, %esp\n\t"
+			"jmp %esp\n\t"
+			);
+		printf(1,"else");
+	}
 }
 
 int main(int argc, char *argv[])
 {
 	signal(SIGFPE, handle_signal);
-	
-	printf(1, "Traps Performed: XXXX\n");
-	printf(1, "Total Elapsed Time: XXXX\n");
-	printf(1, "Average Time Per Trap: XXXXX\n");
+
+	int x = 1;
+	int y = 0;
+	x = x/y;
+
+	time = uptime();
+	float avg = trapCount/time;
+
+	printf(1, "Traps Performed: %d\n", trapCount);
+	printf(1, "Total Elapsed Time: %d\n", time);
+	printf(1, "Average Time Per Trap: %d\n", avg);
 
 	exit();
 }
